8dad67e872331bfd802c81b0c1be1ab2002b9356
fefrry <474241388@qq.com>
2013-07-04 17:18:15 +0800

    after very sad

--- CServerStatusReport.h
+++ CServerStatusReport.h
@@ -1,11 +1,14 @@
-#ifndef _SERVERSPI_H
-#define	_SERVERSPI_H
+#ifndef _SERVERSR_H
+#define	_SERVERSR_H
 
 class CServerStatusReport {
 public:
-	virtual BOOL	GetNetworkState() = 0;
-	virtual LPCTSTR GetServerName() = 0;
-	virtual BOOL	GetServerState() = 0;
+	//获取网络状态
+	virtual BOOL	GetNetworkState()	= 0;
+	//获取服务器名字
+	virtual LPCTSTR GetServerName()		= 0;
+	//获取服务器状态
+	virtual BOOL	GetServerState()	= 0;
 	~CServerStatusReport() {};
 };
-#endif _SERVERSPI_H
+#endif _SERVERSR_H
